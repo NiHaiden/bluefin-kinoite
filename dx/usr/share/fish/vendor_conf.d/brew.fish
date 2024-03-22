7eacce2ffe22ca2be7bb6c09fdc037191ec8bfa5
Niklas <40402114+NiHaiden@users.noreply.github.com>
2024-03-22 00:20:20 +0000

    remove useless line

--- dx/usr/share/fish/vendor_conf.d/brew.fish
+++ dx/usr/share/fish/vendor_conf.d/brew.fish
@@ -1,4 +1,3 @@
 # make shell interactive
 [ -d /home/linuxbrew/.linuxbrew ] && status --is-interactive && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-status --is-interactive && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
